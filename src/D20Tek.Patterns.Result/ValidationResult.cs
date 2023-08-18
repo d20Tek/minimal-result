351e10b0c062de07dd76ff30afe774029aa5cf41
Pedro Silva <pedrosi@microsoft.com>
2023-08-18 14:38:40 -0700

    Added ValidationsResult to help build validation logic in applications. And then convert validation errors into our base Result type. Added unit tests.

---
+++ src/D20Tek.Patterns.Result/ValidationResult.cs
@@ -0,0 +1,61 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+namespace D20Tek.Patterns.Result;
+
+public class ValidationsResult
+{
+    private readonly List<Error> _errors = new();
+
+    public IReadOnlyList<Error> Errors => _errors.AsReadOnly();
+
+    public virtual bool IsValid => _errors.Count == 0;
+
+    public Result<TValue> ToResult<TValue>(TValue? value = null)
+        where TValue : class
+    {
+        if (IsValid)
+        {
+            if (value is null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return value;
+        }
+
+        return _errors;
+    }
+
+    public virtual void AddValidationError(string code, string message)
+    {
+        var error = Error.Validation(code, message);
+        _errors.Add(error);
+    }
+
+    public virtual void AddValidationError(Error error)
+    {
+        GuardInvalidErrorObject(error);
+        _errors.Add(error);
+    }
+
+    public void AddOnFailure(Func<bool> condition, Error error)
+    {
+        ArgumentNullException.ThrowIfNull(condition);
+        GuardInvalidErrorObject(error);
+
+        if (condition() is false)
+        {
+            _errors.Add(error);
+        }
+    }
+
+    private void GuardInvalidErrorObject(Error error)
+    {
+        ArgumentNullException.ThrowIfNull(error);
+        if (error.Type != ErrorType.Validation)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
