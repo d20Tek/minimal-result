fbecbf72aa5314dd83515fca00343a3b89c6da16
Pedro Silva <pedrosi@microsoft.com>
2024-03-25 22:03:05 -0700

    Added Optional<TResult> class to handle returning optional results rather than null. Added unit tests to cover different situations.

---
+++ src/D20Tek.Minimal.Result/Optional.cs
@@ -0,0 +1,77 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+namespace D20Tek.Minimal.Result;
+
+public class Optional<TValue> : IEquatable<Optional<TValue>>
+    where TValue : class
+{
+    private readonly TValue? _value;
+
+    public TValue Value =>
+        _value ?? throw new InvalidOperationException("Value should not be null in this instance.");
+
+    public bool HasValue => _value != null;
+
+    private Optional(TValue? value)
+    {
+        _value = value;
+    }
+
+    public static implicit operator Optional<TValue>(TValue? value) => new(value);
+    
+    public static Optional<TValue> Of(TValue value) => new(value);
+
+    public static Optional<TValue> OfNullable(TValue? value) => new(value);
+
+    public static Optional<TValue> Empty() => new(null);
+
+    public TValue OrElse(TValue other) => HasValue ? Value : other;
+
+    public TValue OrElse(Func<TValue> supplier) => OrElse(supplier());
+
+    public void IfPresentOrElse(Action<TValue> value, Action elseAction)
+    {
+        if (HasValue)
+        {
+            value(Value);
+        }
+        else
+        {
+            elseAction();
+        }
+    }
+
+    public TResult IfPresentOrElse<TResult>(Func<TValue, TResult> value, Func<TResult> elseFunc)
+    {
+        if (HasValue)
+        {
+            return value(Value);
+        }
+        else
+        {
+            return elseFunc();
+        }
+    }
+
+    public override string ToString() => _value?.ToString() ?? string.Empty;
+
+    public override int GetHashCode() => _value?.GetHashCode() ?? 0;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Optional<TValue> other)
+        {
+            return Equals(other);
+        }
+
+        return false;
+    }
+
+    public bool Equals(Optional<TValue>? other)
+    {
+        if (other is null) return false;
+
+        return _value?.Equals(other._value) ?? false;
+    }
+}
---
+++ tests/D20Tek.Minimal.Result.UnitTests/OptionalTests.Overrides.cs
@@ -0,0 +1,173 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using static D20Tek.Minimal.Result.UnitTests.OptionalTests;
+
+namespace D20Tek.Minimal.Result.UnitTests;
+
+[TestClass]
+public class OptionalOverrideTests
+{
+    [TestMethod]
+    public void ToString_WithValue_ResturnsString()
+    {
+        // arrange
+        var t = new Tester { Id = "test-id", Name = "foo" };
+        var v = Optional<Tester>.OfNullable(t);
+
+        // act
+        var result = v.ToString();
+
+        // assert
+        Assert.AreEqual("foo [test-id]", result);
+    }
+
+    [TestMethod]
+    public void ToString_WithNull_ResturnsEmptyString()
+    {
+        // arrange
+        var v = Optional<Tester>.Empty();
+
+        // act
+        var result = v.ToString();
+
+        // assert
+        Assert.AreEqual(string.Empty, result);
+    }
+
+    [TestMethod]
+    public void GetHashcode_WithValue_ResturnsString()
+    {
+        // arrange
+        var t = new Tester { Id = "test-id", Name = "foo" };
+        var v = Optional<Tester>.OfNullable(t);
+
+        // act
+        var result = v.GetHashCode();
+
+        // assert
+        Assert.AreEqual(42, result);
+    }
+
+    [TestMethod]
+    public void GetHashcode_WithNull_ResturnsZero()
+    {
+        // arrange
+        var v = Optional<string>.Empty();
+
+        // act
+        var result = v.GetHashCode();
+
+        // assert
+        Assert.AreEqual(0, result);
+    }
+
+    [TestMethod]
+    public void Equals_WithSameId_ResturnsTrue()
+    {
+        // arrange
+        var t1 = new Tester { Id = "test-id", Name = "foo" };
+        var t2 = new Tester { Id = "test-id", Name = "bar" };
+        var op1 = Optional<Tester>.Of(t1);
+        var op2 = Optional<Tester>.Of(t2);
+
+        // act
+        var result = op1.Equals(op2);
+
+        // assert
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void Equals_WithDifferentId_ResturnsFalse()
+    {
+        // arrange
+        var t1 = new Tester { Id = "test-id", Name = "foo" };
+        var t2 = new Tester { Id = "test-id-2", Name = "foo" };
+        var op1 = Optional<Tester>.Of(t1);
+        var op2 = Optional<Tester>.Of(t2);
+
+        // act
+        var result = op1.Equals(op2);
+
+        // assert
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void Equals_WithNullOther_ResturnsFalse()
+    {
+        // arrange
+        var t1 = new Tester { Id = "test-id", Name = "foo" };
+        var op1 = Optional<Tester>.Of(t1);
+        Optional<Tester>? op2 = null;
+
+        // act
+        var result = op1.Equals(op2);
+
+        // assert
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void Equals_WithEmptyValue_ResturnsFalse()
+    {
+        // arrange
+        var t2 = new Tester { Id = "test-id-2", Name = "foo" };
+        var op1 = Optional<Tester>.Empty();
+        var op2 = Optional<Tester>.Of(t2);
+
+        // act
+        var result = op1.Equals(op2);
+
+        // assert
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void EqualsObject_WithSameId_ResturnsTrue()
+    {
+        // arrange
+        var t1 = new Tester { Id = "test-id", Name = "foo" };
+        var t2 = new Tester { Id = "test-id", Name = "bar" };
+        object op1 = Optional<Tester>.Of(t1);
+        object op2 = Optional<Tester>.Of(t2);
+
+        // act
+        var result = op1.Equals(op2);
+
+        // assert
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void EqualsObject_WithDifferentId_ResturnsFalse()
+    {
+        // arrange
+        var t1 = new Tester { Id = "test-id", Name = "foo" };
+        var t2 = new Tester { Id = "test-id-2", Name = "foo" };
+        object op1 = Optional<Tester>.Of(t1);
+        object op2 = Optional<Tester>.Of(t2);
+
+        // act
+        var result = op1.Equals(op2);
+
+        // assert
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void EqualsObject_WithDifferentType_ResturnsFalse()
+    {
+        // arrange
+        var t1 = new Tester { Id = "test-id", Name = "foo" };
+        object op1 = Optional<Tester>.Of(t1);
+        object op2 = "foo";
+
+        // act
+        var result = op1.Equals(op2);
+
+        // assert
+        Assert.IsFalse(result);
+    }
+}
