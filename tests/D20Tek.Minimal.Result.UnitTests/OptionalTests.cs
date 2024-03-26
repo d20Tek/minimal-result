80964e8b1bce3831dc81d8ff16575b2837995714
DarthPedro <darthpedro@gmail.com>
2024-03-25 22:07:04 -0700

    Adde Optional return type (#16)
    * Added Optional<TResult> class to handle returning optional results rather than null.
    Added unit tests to cover different situations.
    
    * Updated package version numbers.

---
+++ tests/D20Tek.Minimal.Result.UnitTests/OptionalTests.cs
@@ -0,0 +1,190 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using System.Diagnostics.CodeAnalysis;
+
+namespace D20Tek.Minimal.Result.UnitTests;
+
+[TestClass]
+public partial class OptionalTests
+{
+    [TestMethod]
+    [ExcludeFromCodeCoverage]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void Value_WithNull_ThrowsException()
+    {
+        // arrange
+        var opt = Optional<string>.Empty();
+
+        // act
+        _ = opt.Value;
+
+        // assert
+    }
+
+    [TestMethod]
+    public void Implicit_WithStringValue_CreatesOptional()
+    {
+        // arrange
+
+        // act
+        Optional<string> v = "implicit";
+
+        // assert
+        Assert.IsNotNull(v);
+        Assert.IsTrue(v.HasValue);
+        Assert.AreEqual("implicit", v.Value);
+    }
+
+    [TestMethod]
+    public void Of_WithStringValue_CreatesOptional()
+    {
+        // arrange
+
+        // act
+        var v = Optional<string>.Of("test");
+
+        // assert
+        Assert.IsNotNull(v);
+        Assert.IsTrue(v.HasValue);
+        Assert.AreEqual("test", v.Value);
+    }
+
+    [TestMethod]
+    public void OfNullable_WithValue_CreatesOptional()
+    {
+        // arrange
+        var t = new Tester { Id = "test-id", Name = "foo" };
+
+        // act
+        var v = Optional<Tester>.OfNullable(t);
+
+        // assert
+        Assert.IsNotNull(v);
+        Assert.IsTrue(v.HasValue);
+        Assert.AreEqual(t, v.Value);
+    }
+
+    [TestMethod]
+    public void OfNullable_WithNull_CreatesEmptyOptional()
+    {
+        // arrange
+
+        // act
+        var v = Optional<Tester>.OfNullable(null);
+
+        // assert
+        Assert.IsNotNull(v);
+        Assert.IsFalse(v.HasValue);
+    }
+
+    [TestMethod]
+    public void Empty_CreatesEmptyOptional()
+    {
+        // arrange
+
+        // act
+        var v = Optional<Tester>.Empty();
+
+        // assert
+        Assert.IsNotNull(v);
+        Assert.IsFalse(v.HasValue);
+    }
+
+    [TestMethod]
+    public void OrElse_WithValue_ReturnsValue()
+    {
+        // arrange
+        var v = Optional<string>.Of("test");
+
+        // act
+        var result = v.OrElse("none");
+
+        // assert
+        Assert.AreEqual("test", result);
+    }
+
+    [TestMethod]
+    public void OrElse_WithNullValue_ReturnsElseValue()
+    {
+        // arrange
+        var v = Optional<string>.Empty();
+
+        // act
+        var result = v.OrElse("none");
+
+        // assert
+        Assert.AreEqual("none", result);
+    }
+
+    [TestMethod]
+    public void OrElseFunc_WithNullValue_ReturnsElseFuncResult()
+    {
+        // arrange
+        var v = Optional<string>.Empty();
+
+        // act
+        var result = v.OrElse(() => "func-none");
+
+        // assert
+        Assert.AreEqual("func-none", result);
+    }
+
+    [TestMethod]
+    public void IfPresentOrElse_WithValue_ReturnsValue()
+    {
+        // arrange
+        var v = Optional<string>.Of("test");
+        string result = string.Empty;
+
+        // act
+        v.IfPresentOrElse(
+            value => { result = value; },
+            [ExcludeFromCodeCoverage] () => { result = "else"; });
+
+        // assert
+        Assert.AreEqual("test", result);
+    }
+
+    [TestMethod]
+    public void IfPresentOrElse_WithNullValue_ReturnsElseValue()
+    {
+        // arrange
+        var v = Optional<string>.Empty();
+        string result = string.Empty;
+
+        // act
+        v.IfPresentOrElse(
+            [ExcludeFromCodeCoverage] (value) => { result = value; },
+            () => { result = "else"; });
+
+        // assert
+        Assert.AreEqual("else", result);
+    }
+
+    [TestMethod]
+    public void IfPresentOrElseTResult_WithValue_ReturnsValue()
+    {
+        // arrange
+        var v = Optional<string>.Of("test");
+
+        // act
+        var result = v.IfPresentOrElse<bool>(value => true, [ExcludeFromCodeCoverage] () => false);
+
+        // assert
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void IfPresentOrElseTResult_WithNullValue_ReturnsElseValue()
+    {
+        // arrange
+        var v = Optional<string>.Empty();
+
+        // act
+        var result = v.IfPresentOrElse<bool>([ExcludeFromCodeCoverage] (value) => true, () => false);
+
+        // assert
+        Assert.IsFalse(result);
+    }
+}
---
+++ tests/D20Tek.Minimal.Result.UnitTests/Tester.cs
@@ -0,0 +1,36 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using System.Diagnostics.CodeAnalysis;
+
+namespace D20Tek.Minimal.Result.UnitTests;
+
+[ExcludeFromCodeCoverage]
+public class Tester : IEquatable<Tester>
+{
+    public required string Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} [{Id}]";
+    }
+
+    public override int GetHashCode()
+    {
+        return 42;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Tester other)
+        {
+            return Equals(other);
+        }
+
+        return false;
+    }
+
+    public bool Equals(Tester? other) => Id == other?.Id;
+}
